5f428e86329031ff8c005f8c9905b946c45ccc7a
hancyran <1198129562@qq.com>
2019-05-20 21:02:27 +0800

    add test scripts

--- src/models/LGB.py
+++ src/models/LGB.py
@@ -1,7 +1,7 @@
 import lightgbm as lgb
 
 
-def createLGB(max_depth=5, learning_rate=0.1, n_estimators=10000):
+def createLGB(max_depth=5, learning_rate=0.1, n_estimators=10000, feature_fraction=1.0):
     model = lgb.LGBMRegressor(max_depth=max_depth,
                               learning_rate=learning_rate,
                               #                               num_leaves=64,
@@ -12,13 +12,13 @@ def createLGB(max_depth=5, learning_rate=0.1, n_estimators=10000):
                               min_child_weight=1,
                               reg_alpha=0,
                               reg_lambda=5,
-                              gamma=0,
                               scale_pos_weight=1,
                               #                               min_split_gain=0,
                               #                               max_bin=425,
                               #                               subsample_freq=1,
                               seed=2019,
                               boosting_type='gbdt',
+                              feature_fraction= feature_fraction,
                               #                               boosting_type='dart',
                               objective='regression',
                               nthread=32, silent=True)
