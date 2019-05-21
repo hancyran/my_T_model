ce5f2c4596142b0386e7298553c8fbf4dda1660a
hancyran <1198129562@qq.com>
2019-05-21 16:42:15 +0800

    add deepfm model

--- train_LGB_bg.py
+++ train_LGB_bg.py
@@ -11,7 +11,7 @@ learning_rate_list = args.learning_rate
 for i, n in enumerate(learning_rate_list):
     # f.write('setsid python train_LGB_bg.py cv %.4f> %s 2>&1 &\n' % (n, getLogPath('cv', i)))
     # f.write('setsid python train_LGB_bg.py val %.4f> %s 2>&1 &\n' % (n, getLogPath('val', i)))
-    print('Time %d' %d)
+    print('Time %d' %i)
     trainLGB('cv', learning_rate=n)
     trainLGB('val', learning_rate=n)
 
