03b080da9ebe0b5c99756485c4be715f0f34109a
hancyran <1198129562@qq.com>
2019-05-23 00:13:11 +0800

    change args

--- src/feat/data_getter.py
+++ src/feat/data_getter.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 from src.feat.ad_data_getter import getAdData
 from src.feat.user_data_getter import getUserData
-from src.utils.path_args import args
+from src.utils.path_args import pargs
 
 
 def getData(train_type):
@@ -31,8 +31,8 @@ def getData(train_type):
 
 def getFinalData(train_type):
     if train_type == 'cv':
-        if os.path.exists(args.tmp_data_path+'/tmp_train_final_data.npy'):
-            X_train = np.load(args.tmp_data_path+'/tmp_train_final_data.npy')
+        if os.path.exists(pargs.tmp_data_path+'/tmp_train_final_data.npy'):
+            X_train = np.load(pargs.tmp_data_path+'/tmp_train_final_data.npy')
             _, Y_train, test_df = getAdData('cv')
         else:
             X_train, Y_train, test_df = getAdData('cv')
@@ -40,10 +40,10 @@ def getFinalData(train_type):
             X_train = np.concatenate((X_train, X_train_user), axis=1)
         return X_train, Y_train, test_df
     elif train_type == 'val':
-        if os.path.exists(args.tmp_data_path+'/tmp_tri_train_final_data.npy') \
-                and os.path.exists(args.tmp_data_path+ '/tmp_tri_train_final_data.npy'):
-            X_train = np.load(args.tmp_data_path+'/tmp_tri_train_final_data.npy')
-            X_test = np.load(args.tmp_data_path+'/tmp_tri_train_final_data.npy')
+        if os.path.exists(pargs.tmp_data_path+'/tmp_tri_train_final_data.npy') \
+                and os.path.exists(pargs.tmp_data_path+ '/tmp_tri_train_final_data.npy'):
+            X_train = np.load(pargs.tmp_data_path+'/tmp_tri_train_final_data.npy')
+            X_test = np.load(pargs.tmp_data_path+'/tmp_tri_train_final_data.npy')
             _, Y_train, _, Y_test, test_df = getAdData('val')
 
         else:
@@ -53,9 +53,9 @@ def getFinalData(train_type):
             X_test = np.concatenate((X_test, X_test_user), axis=1)
         return X_train, Y_train, X_test, Y_test, test_df
     elif train_type == 'test':
-        if os.path.exists(args.tmp_data_path+'/tmp_train_final_data.npy'):
-            X_train = np.load(args.tmp_data_path+'/tmp_train_final_data.npy')
-            X_test = np.load(args.tmp_data_path+'/tmp_test_final_data.npy')
+        if os.path.exists(pargs.tmp_data_path+'/tmp_train_final_data.npy'):
+            X_train = np.load(pargs.tmp_data_path+'/tmp_train_final_data.npy')
+            X_test = np.load(pargs.tmp_data_path+'/tmp_test_final_data.npy')
             _, Y_train, _, test_df = getAdData('test')
         else:
             X_train, Y_train, X_test, test_df = getAdData('test')
@@ -69,16 +69,16 @@ def getFinalData(train_type):
 
 def getData_feat2(train_type):
     if train_type == 'cv':
-        X_train = pd.read_hdf(args.tmp_data_path+'/tmp_train_feat2_ad.h5').values
+        X_train = pd.read_hdf(pargs.tmp_data_path+'/tmp_train_feat2_ad.h5').values
         X_train = np.delete(X_train, 7, axis=1)
-        Y_train = np.load(args.train_data_path+'/train_arr_label.npy')
-        test_df = pd.read_hdf(args.train_data_path+'/train_origin_final.h5')
+        Y_train = np.load(pargs.train_data_path+'/train_arr_label.npy')
+        test_df = pd.read_hdf(pargs.train_data_path+'/train_origin_final.h5')
         return X_train, Y_train, test_df
     elif train_type == 'val':
-        X_train = pd.read_hdf(args.tmp_data_path+'/tmp_train_feat2_ad.h5').values
+        X_train = pd.read_hdf(pargs.tmp_data_path+'/tmp_train_feat2_ad.h5').values
         X_train = np.delete(X_train, 7, axis=1)
-        Y_train = np.load(args.train_data_path+'/train_arr_label.npy')
-        test_df = pd.read_hdf(args.train_data_path+'/train_origin_final.h5')
+        Y_train = np.load(pargs.train_data_path+'/train_arr_label.npy')
+        test_df = pd.read_hdf(pargs.train_data_path+'/train_origin_final.h5')
         # pepare for dataset
         i = test_df.loc[test_df.日期 == 319].index
         m = test_df.loc[test_df.日期 != 319].index
@@ -90,7 +90,7 @@ def getData_feat2(train_type):
         Y_val_label = Y_train[i]
         return X_tri_data, Y_tri_label, X_val_data, Y_val_label, test_df
     elif train_type == 'test':
-        X_train = pd.read_hdf(args.tmp_data_path+'/tmp_train_feat2_ad.h5').values
+        X_train = pd.read_hdf(pargs.tmp_data_path+'/tmp_train_feat2_ad.h5').values
         X_train = np.delete(X_train, 7, axis=1)
         Y_train = np.load('train2/train_arr_label.npy')
         test_df = pd.read_hdf('test2/test_origin_final.h5')
@@ -104,14 +104,14 @@ def getData_feat2(train_type):
 
 def getData_feat3(train_type):
     if train_type == 'cv':
-        X_train = np.load(args.tmp_data_path+'/tmp_train_feat3_withlen.npy')
-        Y_train = np.load(args.train_data_path+'/train_arr_label.npy')
-        test_df = pd.read_hdf(args.train_data_path+'/train_origin_final.h5')
+        X_train = np.load(pargs.tmp_data_path+'/tmp_train_feat3_withlen.npy')
+        Y_train = np.load(pargs.train_data_path+'/train_arr_label.npy')
+        test_df = pd.read_hdf(pargs.train_data_path+'/train_origin_final.h5')
         return X_train, Y_train, test_df
     elif train_type == 'val':
-        X_train = np.load(args.tmp_data_path+'/tmp_train_feat3_withlen.npy')
-        Y_train = np.load(args.train_data_path+'/train_arr_label.npy')
-        test_df = pd.read_hdf(args.train_data_path+'/train_origin_final.h5')
+        X_train = np.load(pargs.tmp_data_path+'/tmp_train_feat3_withlen.npy')
+        Y_train = np.load(pargs.train_data_path+'/train_arr_label.npy')
+        test_df = pd.read_hdf(pargs.train_data_path+'/train_origin_final.h5')
         # pepare for dataset
         i = test_df.loc[test_df.日期 == 319].index
         m = test_df.loc[test_df.日期 != 319].index
@@ -123,11 +123,11 @@ def getData_feat3(train_type):
         Y_val_label = Y_train[i]
         return X_tri_data, Y_tri_label, X_val_data, Y_val_label, test_df
     elif train_type == 'test':
-        X_train = np.load(args.tmp_data_path+'/tmp_train_feat3_withlen.npy')
-        Y_train = np.load(args.train_data_path+'/train_arr_label.npy')
-        test_df = pd.read_hdf(args.test_data_path+'/test_origin_final.h5')
+        X_train = np.load(pargs.tmp_data_path+'/tmp_train_feat3_withlen.npy')
+        Y_train = np.load(pargs.train_data_path+'/train_arr_label.npy')
+        test_df = pd.read_hdf(pargs.test_data_path+'/test_origin_final.h5')
 
-        X_test = np.load(args.tmp_data_path+'/tmp_test_feat3_withlen.npy')
+        X_test = np.load(pargs.tmp_data_path+'/tmp_test_feat3_withlen.npy')
 
         return X_train, Y_train, X_test, test_df
     else:
